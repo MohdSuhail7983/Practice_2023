a1517f3fd7fd4aacbfa51be0e2f8e095456eb1d5
mohd suhail <suhailgaur9181@gmail.com>
2023-06-17 16:41:54 +0530

    commit 1 : main : Added module1/Day1

--- module1/Day1/question2.c
+++ module1/Day1/question2.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
- int main(){
-    printf("hello world");
-    return 0;
+
+int sum(int a,int b);
+
+
+int main(){
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+
+    printf("enter the second number:");
+    scanf("%d",&b);
+
+    int s = sum(a,b);
+    printf("the value of s is %d :",s);
+
+
+return 0;
+}
+int sum(int a,int b){
+    return a+b;
 }
